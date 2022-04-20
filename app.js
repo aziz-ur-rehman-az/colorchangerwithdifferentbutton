08a97ebaca7bf4beb6fe37754a369cf64e87ab0d
aziz-ur-rehman-az <azizrehmanonly@gmail.com>
2022-04-20 12:50:42 +0500

    initialcommit

---
+++ app.js
@@ -0,0 +1,30 @@
+function red() {
+  document.getElementById("red");
+  document.getElementById("output").innerHTML =
+    "<img src='red.svg' width = 300px/>";
+  //   document.getElementById("output").style.backgroundColor = "red";
+}
+function blue() {
+  document.getElementById("blue");
+  document.getElementById("output").innerHTML =
+    "<img src='blue.svg' width = 300px />";
+  //   document.getElementById("output").style.backgroundColor = "blue";
+}
+function green() {
+  document.getElementById("green");
+  document.getElementById("output").innerHTML =
+    "<img src='green.svg' width = 300px />";
+  //   document.getElementById("output").style.backgroundColor = "green";
+}
+function black() {
+  document.getElementById("black");
+  document.getElementById("output").innerHTML =
+    "<img src='black.svg' width = 300px />";
+  //   document.getElementById("output").style.backgroundColor = "black";
+}
+function yellow() {
+  document.getElementById("yellow");
+  document.getElementById("output").innerHTML =
+    "<img src='yellow.svg' width = 300px />";
+  //   document.getElementById("output").style.backgroundColor = "yellow";
+}
